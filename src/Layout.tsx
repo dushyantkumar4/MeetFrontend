d25aeb0036182f78fc642220c43501331bee4ea1
dushyant kumar <dushyantvelar@gmail.com>
2026-03-12 16:29:31 +0530

    design the home page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,11 +2,11 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
 const Layout = () => {
   return (
-    <div>
-        <Navbar/>
-        <Outlet/>
+    <div className="w-full h-screen  bg-cover bg-center bg-no-repeat">
+      <Navbar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
